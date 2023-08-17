7563c4fafc815d1cc089222e16374392a5af8c60
Crowdbotics <team@crowdbotics.com>
2023-08-17 22:37:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-sound-42915",
+  url: "https://fragrant-sound-42915.botics.co",
+  api: "https://fragrant-sound-42915.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
